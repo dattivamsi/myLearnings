9511d2b0f935b00583179fe9f5c25787108547a4
dattivamsi <dattitarun@gmail.com>
2023-08-12 15:13:33 +0530

    css hover eff1

---
+++ src/Buttons/HoverButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './HoverButton.css'
+
+function HoverButton() {
+  return (
+    <div className="button_container">
+      <button>BUTTON</button>
+    </div>
+  );
+}
+
+export default HoverButton;
