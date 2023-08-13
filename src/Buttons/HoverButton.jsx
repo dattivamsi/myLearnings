818d719afedac8d521e4a05f0d6e239847506fa7
dattivamsi <dattitarun@gmail.com>
2023-08-13 13:33:43 +0530

    css practice

--- src/Buttons/HoverButton.jsx
+++ src/Buttons/HoverButton.jsx
@@ -5,6 +5,16 @@ function HoverButton() {
   return (
     <div className="button_container">
       <button>BUTTON</button>
+      <button className="botder-effect">button
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <button className="hover_message"></button>
+      <button className="folded_button">button</button>
     </div>
   );
 }
